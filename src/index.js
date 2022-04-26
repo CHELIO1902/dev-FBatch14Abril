f088920d7ff6e6661a5dfae8ce780bb368e749ca
CHELIO1902 <chelio19022012@hotmail.com>
2022-04-26 18:56:36 -0500

    apuntes y primeros ejercicios de la clase 25/04/2022

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// console.log('hola mundo')
+
+// crear una funcion que reciba dos numeros y ejecutarlos con node
+
+// function suma (a, b) {
+//   var suma = num1 + num2
+// }
+
+// console.log(suma(3, 4))
+
+// const suma = (a, b) => {
+//   return a + b;
+// };
+
+// console.log(suma(5, 5));
+
+// const suma = (num1, num2) => {console.log(num1 + num2)}
+// suma(6, 7)
+
+//Creen al menos un json sobre una banda de musica y que lo validen con jsonlint
+//ademas, agregar un script a los scripts de package, y ejecutarlo con npm run
